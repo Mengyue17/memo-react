02b7e97d69020a0c795522572440c2b9f5f16904
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-05-18 12:10:55 +0000

    testing AUs

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -4,4 +4,9 @@ class Stock < ActiveRecord::Base
 validates_presence_of :symbol
  
  def validate
-	errors.add(:symbol, "Must be a v
+	errors.add(:symbol, "Must be a valid Ticker Symbol") if date.nil?|| date == "invalid date"
+  end
+
+
+  def Stock::getStock(ticker)
+	# Special Charac
