6bafe85c9ed4f0577d8afcb8d422da0ee76da43b
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-05-21 08:06:10 +0000

    Fixed codecheck.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -9,4 +9,6 @@ validates_presence_of :symbol
 
 
   def Stock::getStock(ticker)
-	# Special Characters:d1=date,t1=Last Trade Time, s=Symbol, n=Name, g=Day's Low, l1=Last Trade (Price Only), p2=Percent Change, h=Day's High, x=Stock Exchange (ref:http://www.gummy-stuff.org/Yaho
+	# Special Characters:d1=date,t1=Last Trade Time, s=Symbol, n=Name, g=Day's Low, l1=Last Trade (Price Only), p2=Percent Change, h=Day's High, x=Stock Exchange (ref:http://www.gummy-stuff.org/Yahoo-data.htm)
+        Net::HTTP.get('download.finance.yahoo.com','/d?f=d1t1sngl1hx&s='+ ticker);
+ 
