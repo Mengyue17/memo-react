114b3d64a4f7e8ee7fa2fe8bc73e863f5471f496
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-05-22 15:48:41 +0000

    Add result

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -11,4 +11,8 @@ validates_presence_of :symbol
   def Stock::getStock(ticker)
 	# Special Characters:d1=date,t1=Last Trade Time, s=Symbol, n=Name, g=Day's Low, l1=Last Trade (Price Only), p2=Percent Change, h=Day's High, x=Stock Exchange (ref:http://www.gummy-stuff.org/Yahoo-data.htm)
         Net::HTTP.get('download.finance.yahoo.com','/d?f=d1t1sngl1hx&s='+ ticker);
- 
+  end
+
+  def self.search(search)
+    if search
+      find(:all, :conditions =>['symbol LIKE ?', "%#{sear
