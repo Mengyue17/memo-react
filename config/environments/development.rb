2c45072d2888793f79732883bae9289724eb1e02
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-07-17 18:31:04 +0000

    Update History

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,4 +10,6 @@ StockAppGT::Application.configure do
   config.whiny_nils = true
 
   # Show full error reports and disable caching
-  config.c
+  config.consider_all_requests_local       = false
+  config.action_view.debug_rjs             = false
+  conf
