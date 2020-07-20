2453e86e596897ddb607febb188800f0a80c70c4
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-07-20 19:13:58 +0000

    Update Travis notification email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,4 +17,7 @@ StockAppGT::Application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
-  # Print deprecation notices to t
+  # Print deprecation notices to the Rails logger
+  config.active_support.deprecation = :log
+
+  # Only use best
