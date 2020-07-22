ea454b3a3d41ce401e5511fb74e9d6a3ef61ad93
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-07-22 10:30:18 +0000

    try fix build

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,2 +1,4 @@
 StockAppGT::Application.configure do
-  # Settings specified here will take precedence ove
+  # Settings specified here will take precedence over those in config/application.rb
+
+  # The production environment is meant for finished, 
