e0d945b47f8ebda761233a108fe41e7aa3b1654a
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-07-26 06:48:57 +0000

    Update documents.html

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,4 +6,7 @@ StockAppGT::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_lo
+  config.consider_all_requests_local       = false
+  config.action_controller.perform_caching = true
+
+  # Specifies the hea
