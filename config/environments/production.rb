ccb17e0a23897112e4fc7a7ea6abb99359ac0fde
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-09-21 14:30:57 +0000

    edited .gitignore

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,4 +28,7 @@ StockAppGT::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Disable Rails's static asset server
-  # In production, Apache or ngin
+  # In production, Apache or nginx will already do this
+  config.serve_static_assets = false
+
+  # Enable serving o
