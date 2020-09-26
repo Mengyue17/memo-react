a700b04f0c7960cfb7088178cd0ee006bc5e975e
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-09-26 10:35:45 +0000

    added new random number

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,4 +40,7 @@ StockAppGT::Application.configure do
   # Enable threaded mode
   # config.threadsafe!
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall bac
+  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # the I18n.default_locale when a translation can not be found)
+  config.i18n.fallbacks = true
+
