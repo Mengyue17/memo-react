3617336b4fd5c36e38cfb0d058ca49049ac6f435
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-09-26 13:13:43 +0000

    dcb v0.0.17. extract dcb-os.yml from .travis.yml. rewrite .travis.yml.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,4 +45,5 @@ StockAppGT::Application.configure do
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
-  config.active_support.deprecation 
+  config.active_support.deprecation = :notify
+end
