d104689840041d9406c1e52d1b22bb2144056552
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-09-26 13:06:02 +0000

    ddddd

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,3 +44,5 @@ StockAppGT::Application.configure do
   # the I18n.default_locale when a translation can not be found)
   config.i18n.fallbacks = true
 
+  # Send deprecation notices to registered listeners
+  config.active_support.deprecation 
