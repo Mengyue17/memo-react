ff92bdcee3a45bfd9f00e4043b31bca07389ad74
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-10-27 10:16:19 +0000

    Obsolete.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,3 @@
-# Be sure to restart your server when you modify this
+# Be sure to restart your server when you modify this file.
+
+StockAppGT::Application.config.session_store :cookie_store, :key => 
