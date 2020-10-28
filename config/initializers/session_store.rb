4bcbe902df3ee703282b590f91db70972c7d763d
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-10-28 08:15:59 +0000

    update numbers

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-StockAppGT::Application.config.session_store :cookie_store, :key => 
+StockAppGT::Application.config.session_store :cookie_store, :key => '_StockAppGT_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# whic
