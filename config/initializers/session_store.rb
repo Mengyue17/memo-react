1fd44f1f2c504d42c8bbcca8ffbe42911366ed8d
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-11-22 17:09:29 +0000

    Update advcl.md

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,4 +5,4 @@ StockAppGT::Application.config.session_store :cookie_store, :key => '_StockAppGT
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# StockAppGT::Application.config.session_sto
+# StockAppGT::Application.config.session_store :active_record_store
