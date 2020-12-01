d870902f172e73b146cf12b5de16f971307df54d
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2020-12-01 06:51:00 +0000

    Removing unused methods

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+
+StockAppGT::Application.routes.draw do
+  resources :stocks
+
+  # The priority is based upon order of creation:
+  # first created -> highest priority.
+
+  # Sample of regular route:
+  #   match 'products/:id' => 'catalog#view'
+  # Keep in mind you can assign values other than :controller and :action
+
+
+  # Sample of named route:
+  #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
+  # This route can be invoked with purchase_url(:id => product.id)
+
+
+  # Sample resource route (maps HTTP verbs to controller actions automatically):
+  #   resources :products
+
+  # Sample resource route with options:
+  #   resources :products do
+  #     member do
+  #       get 'short'
