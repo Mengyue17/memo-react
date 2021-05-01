6e6049d7499e9e0740275487fe8c62d0dde41420
Mengyue17 <122732260+Mengyue17@users.noreply.github.com>
2021-05-01 05:45:08 +0000

    Update academy.md

--- public/javascripts/effects.js
+++ public/javascripts/effects.js
@@ -38,4 +38,8 @@ Element.collectTextNodesIgnoreClass = function(element, className) {
   return $A($(element).childNodes).collect( function(node) {
     return (node.nodeType==3 ? node.nodeValue :
       ((node.hasChildNodes() && !Element.hasClassName(node,className)) ?
-        Element.collectTextNodesI
+        Element.collectTextNodesIgnoreClass(node, className) : ''));
+  }).flatten().join('');
+};
+
+Element.setContentZoom = func
